66c45caa47e0d66089b0e44c48f7440f35467d1d
aly511 <alyxmomdouh@gmail.com>
2022-02-19 21:21:58 +0200

    Add project files.

---
+++ EFECORE/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFECORE
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ EFECORE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFECORE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
