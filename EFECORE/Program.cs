9405be01e91a6b795e504fb856d1a7501932e084
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2022-04-09 13:37:24 +0200

    v56 entity framework core -- Explicit Loading

--- EFECORE/Program.cs
+++ EFECORE/Program.cs
@@ -32,9 +32,19 @@ namespace EFECORE
                 Console.WriteLine($"{tracker.Entity.ToString()} ,{tracker.State.ToString()}, {tracker.CurrentValues}");
             }
             // example v55 entity framework core -- Eager Loading
-            var blog3 = context.Blogs.Include(x=>x.BlogImage.Caption).SingleOrDefault(l=>l.Id==1);
-            //Eager Loading with nested navigation properties
-            var blog32 = context.Blogs.Include(bi=>bi.BlogImage).ThenInclude(x=>x.Blog).SingleOrDefault(l=>l.Id==1);
+                var blog3 = context.Blogs.Include(x=>x.BlogImage.Caption).SingleOrDefault(l=>l.Id==1);
+                //Eager Loading with nested navigation properties
+                var blog32 = context.Blogs.Include(bi=>bi.BlogImage).ThenInclude(x=>x.Blog).SingleOrDefault(l=>l.Id==1);
+            // Explicit Loading only load navigation properties when we need it in 2 steps not 1 step like Eager Loading
+                var blog41 = context.Blogs.SingleOrDefault(l => l.Id == 1);
+                 context.Entry(blog41).Reference(b=>b.BlogImage).Load();// if one property
+                 var checkifloaded=context.Entry(blog41).Reference(b=>b.BlogImage).IsLoaded;// bool
+                 var checkIsModified = context.Entry(blog41).Reference(b=>b.BlogImage).IsModified;//bool
+            // if navigation properties is Icollection or List
+            var blog42 = context.Blogs.SingleOrDefault(l => l.Id == 1);
+            context.Entry(blog42).Collection(b => b.Posts).Load(); 
+            context.Entry(blog42).Collection(b => b.Posts).Query().Where(x=>x.Id==2).ToList();//make query on list navigation properties
+            context.Entry(blog42).Collection(b => b.Posts).Query().Count(); 
         }
         // function to seeddata to database 
         public static void SeedData()
